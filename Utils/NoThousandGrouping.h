4591779ceeb767c3252d52e3081cc42bd7a8442f
flo <finkf@cis.uni-muenchen.de>
2016-03-07 17:40:04 +0100

    add custom facet with `.` decimal separator

--- Utils/NoThousandGrouping.h
+++ Utils/NoThousandGrouping.h
@@ -5,6 +5,11 @@ namespace OCRCorrection {
   protected:
     wchar_t do_thousands_sep() const /*override*/ {return L'\0';}
   };
+
+  struct NoThousandGroupingWithDecimalDot: public NoThousandGrouping {
+  protected:
+    wchar_t do_decimal_point() const /*override*/ {return L'.';}
+  };
 }
 
 #endif // OCRCorrection_NoThousandGrouping_h__
