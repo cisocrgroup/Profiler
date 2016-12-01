d1db19bd39e9f74253f24108715a7435b7ada0ae
flo <finkf@cis.uni-muenchen.de>
2016-12-01 15:15:03 +0100

    redirect cout and cerr to log file for better debugging

--- Profiler/AdaptiveProfiling.cxx
+++ Profiler/AdaptiveProfiling.cxx
@@ -6,5 +6,14 @@ using namespace OCRCorrection;
 void
 Profiler::createAdaptiveProfile(Document& sourceDoc)
 {
+	std::wofstream os("/tmp/adaptive-profiler.log");
+	if (not os.good())
+		throw std::system_error(errno, std::system_category(), "/tmp/adaptive-profiler.log");
+	auto oldcout = std::wcout.rdbuf(os.rdbuf());
+	auto oldcerr = std::wcerr.rdbuf(os.rdbuf());
+
 	createNonAdaptiveProfile(sourceDoc);
+
+	std::wcout.rdbuf(oldcout);
+	std::wcerr.rdbuf(oldcerr);
 }
