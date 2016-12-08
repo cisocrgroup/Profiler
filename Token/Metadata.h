fe418083728bbb21c21d86ad56874aab6c388cf6
flo <finkf@cis.uni-muenchen.de>
2016-12-08 13:45:43 +0100

    add basic implementation for Metadata

---
+++ Token/Metadata.h
@@ -0,0 +1,42 @@
+#ifndef OCRCorrection_Metadata_hpp__
+#define OCRCorrection_Metadata_hpp__
+
+#include <map>
+
+namespace OCRCorrection {
+	class Token;
+	class Metadata {
+	public:
+		enum class Type {
+			AdaptiveGtToken,
+			EvaluationGtToken,
+		};
+
+		Metadata(Token& token)
+			: map_()
+			, token_(&token)
+		{}
+
+		const Token& token() const noexcept {
+			return *token_;
+		}
+		Token& token() noexcept {
+			return *token_;
+		}
+		const std::wstring& operator[](Type type) const {
+			return map_.at(type);
+		}
+		std::wstring& operator[](Type type) {
+			return map_[type];
+		}
+		bool has(Type type) const noexcept {
+			return map_.count(type);
+		}
+
+	private:
+		std::map<Type, std::wstring> map_;
+		Token* token_;
+	};
+}
+
+#endif // OCRCorrection_Metadata_hpp__
