2efdb03e8e70d5ebc4027f17609a93096dc0b94f
flo <finkf@cis.uni-muenchen.de>
2016-12-09 17:36:57 +0100

    improve base word frequency smoothing

--- Profiler/FrequencyList.h
+++ Profiler/FrequencyList.h
@@ -142,13 +142,10 @@ namespace OCRCorrection {
 
 	float getBaseWordFrequency( std::wstring const& word ) const {
 	    float f = 0;
-	    if( baseWordFrequency_->lookup( word, &f) ) {
-		return f;
-	    }
-	    else {
-		// TRIVIAL SMOOTHING FOR BASEWORD FREQUENCIES
-		return 1;
-	    }
+	    baseWordFrequency_->lookup(word, &f);
+	    if (f <= 0)
+		f = 1; // TRIVIAL SMOOTHING FOR BASEWORD FREQUENCIES
+	    return f;
 	}
 
 	size_t getInterpretationCount( csl::Interpretation const& interp ) const {
