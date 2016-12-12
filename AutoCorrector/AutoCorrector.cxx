8170218795b4e9a45ac19bca2d1c48bf4977a51a
flo <finkf@cis.uni-muenchen.de>
2016-12-12 16:53:39 +0100

    implement correction of first XAutoCorrector/AutoCorrector.cxx of tokens

--- AutoCorrector/AutoCorrector.cxx
+++ AutoCorrector/AutoCorrector.cxx
@@ -1,3 +1,4 @@
+#include <regex>
 #include "AutoCorrector.h"
 #include "Document/Document.h"
 #include "Utils/Utils.h"
@@ -8,13 +9,18 @@ using namespace OCRCorrection;
 void
 AutoCorrector::add_patterns(const std::string& pats)
 {
-	size_t i = -1;
-	do {
-		++i;
-		const auto pos = pats.find(",", i);
-		add_pattern(pats.substr(i, pos -i));
-		i = pos;
-	} while (i != std::string::npos);
+	std::smatch m;
+	if (std::regex_match(pats, m, std::regex(R"(first\s*(\d{1,2})%)"))) {
+		percent_ = (double) std::stoi(m[1]) / 100.0;
+	} else {
+		size_t i = -1;
+		do {
+			++i;
+			const auto pos = pats.find(",", i);
+			add_pattern(pats.substr(i, pos -i));
+			i = pos;
+		} while (i != std::string::npos);
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -33,6 +39,29 @@ AutoCorrector::add_pattern(const std::string& pat)
 ////////////////////////////////////////////////////////////////////////////////
 void
 AutoCorrector::correct(Document& doc) const
+{
+	if (not patterns_.empty())
+		correct(doc, CorrectPatterns());
+	else
+		correct(doc, CorrectPercent());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+AutoCorrector::correct(Document& doc, CorrectPercent) const
+{
+	int n = static_cast<int>(doc.getNrOfTokens() * percent_);
+	for (auto& token: doc) {
+		if (n <= 0)
+			break;
+		correct(token);
+		--n;
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+AutoCorrector::correct(Document& doc, CorrectPatterns) const
 {
 	for (auto& token: doc) {
 		if (not token.isNormal()) // skip not normal token
@@ -60,7 +89,7 @@ AutoCorrector::correct(Token& token) const
 {
 	token.metadata()["correction"] = token.metadata()["groundtruth"];
 	token.metadata()["correction-lc"] = token.metadata()["groundtruth-lc"];
-	// std::wcout << "Correcting token " << token.getWOCR()
-	// 	   << " with "
-	// 	   << token.metadata()["correction"] << "\n";
+	std::wcout << "Correcting token " << token.getWOCR()
+		   << " with "
+		   << token.metadata()["correction"] << "\n";
 }
