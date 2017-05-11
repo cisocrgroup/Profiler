2ec9b5cd83ca364c486c7d9aec2b786e57420a36
flo <finkf@cis.uni-muenchen.de>
2017-05-11 11:57:39 +0200

    first (basic) impl of approximate lookup

--- tools/lookupMD/lookupMD.cxx
+++ tools/lookupMD/lookupMD.cxx
@@ -1,10 +1,29 @@
-#include<cstdlib>
+#include <cstdlib>
+#include <errno.h>
 #include "MinDic/MinDic.h"
 #include "FBDic/FBDic.h"
 #include "Getopt/Getopt.h"
-#include<errno.h>
+#include "LevFilter/LevFilter.h"
+#include "MSMatch/MSMatch.h"
 
 using namespace csl;
+
+class MyCandidateReceiver: public csl::LevFilter::CandidateReceiver {
+public:
+	MyCandidateReceiver(): found_(false) {}
+	virtual ~MyCandidateReceiver() override = default;
+	virtual void receive(const wchar_t*, int, int) override {
+		found_ = true;
+	}
+	virtual void reset() override {
+		found_ = false;
+	}
+	bool found() const noexcept {
+		return found_;
+	}
+private:
+	bool found_;
+};
 
 int main( int argc, char const** argv ) {
     std::locale::global( std::locale("") ); // set the environment's default locale
@@ -12,15 +31,16 @@ int main( int argc, char const** argv ) {
     try {
 
         csl::Getopt options;
+	options.specifyOption("k", csl::Getopt::STRING);
         options.getOptionsAsSpecified( argc, argv );
 
-	if(argc != 2) {
-	    std::cerr<<"Use like: "<<argv[0]<<" <binDic>"<<std::endl;
+	if(argc < 2) {
+	    std::cerr<<"Use like: " << argv[0] << " <binDic> [--k n]"<<std::endl;
 	    exit(1);
 	}
 
 
-
+	int k = 0;
 	csl::MinDic<> const* minDic = 0;
 	csl::FBDic<>* fbdic = 0; // this one is loaded in case a fbdic is passed to the program
 
@@ -33,20 +53,34 @@ int main( int argc, char const** argv ) {
 	    minDic = new csl::MinDic<>( options.getArgument( 0 ).c_str() );
 	}
 
+	if (options.hasOption("k")) {
+		k = std::stoi(options.getOption("k"));
+	}
 
+	csl::MSMatch<> msmatch(k, options.getArgument(0).c_str());
+	MyCandidateReceiver r;
 
 	std::wstring query;
 	while( std::getline( std::wcin, query ).good() ) {
 	    // is this really necessary ??
 	    if ( query.length() > Global::lengthOfLongStr ) {
-		throw exceptions::badInput( "csl::lookupMD: Maximum length of input line violated (set by Global::lengthOfLongStr)" );
+		throw exceptions::badInput("csl::lookupMD: Maximum length of input line violated (set by Global::lengthOfLongStr)" );
 	    }
 
 	    int ann = 0;
-	    if( minDic->lookup( query.c_str(), &ann ) ) {
-		std::wcout<<ann<<std::endl;
+	    if (k == 0) {
+		    if( minDic->lookup( query.c_str(), &ann ) ) {
+			std::wcout << ann << std::endl;
+		    }
+		    else std::wcout <<"-1" << std::endl;
+	    } else {
+		    msmatch.query(query.c_str(), r);
+		    if (r.found())
+			    std::wcout << "1\n";
+		    else
+			    std::wcout << "-1\n";
+		    r.reset();
 	    }
-	    else std::wcout<<std::endl;
 	}
 	if( errno == EILSEQ ) {
 	    throw exceptions::badInput( "csl::lookupMD: Input encoding error" );
@@ -58,3 +92,4 @@ int main( int argc, char const** argv ) {
     }
     return 0;
 }
+
