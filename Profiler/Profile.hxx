e02da2bf3667f147662a935f48d2397af3a2ba38
flo <flo@cis.lmu.de>
2019-09-24 13:31:41 +0200

    Indentation

--- Profiler/Profile.hxx
+++ Profiler/Profile.hxx
@@ -7,8 +7,7 @@
 #include <unordered_map>
 
 namespace csl {
-template<class T>
-class MinDic;
+template <class T> class MinDic;
 }
 
 namespace OCRCorrection {
@@ -17,37 +16,34 @@ class Profile;
 class LanguageModel;
 class PatternCounter;
 
-class Profile
-{
+class Profile {
 public:
-  struct TokenType
-  {
+  struct TokenType {
     static const std::wstring NOT_NORMAL, DONT_TOUCH, SHORT, NORMAL,
-      WAS_PROFILED, UNKNOWN, GUESSED_HIST_TRACE_NONEMPTY,
-      GUESSED_OCR_TRACE_NONEMPTY;
+        WAS_PROFILED, UNKNOWN, GUESSED_HIST_TRACE_NONEMPTY,
+        GUESSED_OCR_TRACE_NONEMPTY;
   };
-  using F = std::function<void(const Token&, csl::DictSearch::CandidateSet&)>;
-  using Tuple = std::tuple<size_t,
-                           csl::DictSearch::CandidateSet,
+  using F = std::function<void(const Token &, csl::DictSearch::CandidateSet &)>;
+  using Tuple = std::tuple<size_t, csl::DictSearch::CandidateSet,
                            std::vector<WeightedCandidate>>;
   using Map = std::unordered_map<std::wstring, Tuple>;
 
   Profile() = default;
-  void put(const Token& token, F f);
-  void iteration(const LanguageModel& lm);
+  void put(const Token &token, F f);
+  void iteration(const LanguageModel &lm);
   void finish();
-  void setCorrection(Token& token) const;
+  void setCorrection(Token &token) const;
   int iteration() const { return iteration_; }
 
 private:
-  void updateGlobalHistPatterns(const LanguageModel& lm,
-                                const PatternCounter& counts) const;
-  void updateGlobalOCRPatterns(const LanguageModel& lm,
-                               const PatternCounter& counts) const;
-  void updateDictDistributions(const LanguageModel& lm) const;
-  void updateCounts(const Token& token);
-  void updateBaseWordFrequencies(const LanguageModel& lm,
-                                 const std::map<std::wstring, double>& m);
+  void updateGlobalHistPatterns(const LanguageModel &lm,
+                                const PatternCounter &counts) const;
+  void updateGlobalOCRPatterns(const LanguageModel &lm,
+                               const PatternCounter &counts) const;
+  void updateDictDistributions(const LanguageModel &lm) const;
+  void updateCounts(const Token &token);
+  void updateBaseWordFrequencies(const LanguageModel &lm,
+                                 const std::map<std::wstring, double> &m);
   bool skipCutoff(double count, double cutoff) const;
 
   Map types_;
@@ -56,6 +52,6 @@ private:
   size_t ocrCharacterCount_;
   int iteration_;
 };
-}
+} // namespace OCRCorrection
 
 #endif // OCRC_PROFILE_HXX
