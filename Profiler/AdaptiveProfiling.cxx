2ae5e6769a6cedab1bc455ae6c58289fdb5d6441
flo <flo@cis.lmu.de>
2019-09-24 13:43:17 +0200

    Add debug for addaptive tokens

--- Profiler/AdaptiveProfiling.cxx
+++ Profiler/AdaptiveProfiling.cxx
@@ -4,23 +4,22 @@
 using namespace OCRCorrection;
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-Profiler::calculateCandidateSet(const Token& t,
-                                csl::DictSearch::CandidateSet& candidates)
-{
+void Profiler::calculateCandidateSet(
+    const Token &t, csl::DictSearch::CandidateSet &candidates) {
+  // std::wcerr << "(Profiler) Calculating candidate set for token " <<
+  // t.getWOCR()
+  //            << "\n";
   candidates.reset();
-  if (config_.adaptive_ and t.has_metadata("correction"))
+  if (config_.adaptive_ and t.has_metadata("correction")) {
     calculateAdaptiveCandidateSet(t, candidates);
-  else
+  } else {
     calculateNonAdaptiveCandidateSet(t, candidates);
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-Profiler::calculateNonAdaptiveCandidateSet(
-  const Token& t,
-  csl::DictSearch::CandidateSet& candidates)
-{
+void Profiler::calculateNonAdaptiveCandidateSet(
+    const Token &t, csl::DictSearch::CandidateSet &candidates) {
   // std::wcerr << "(Profiler) Calculating non adaptive profile for token "
   //            << t.getWOCR() << "\n";
   dictSearch_.query(t.getWOCR_lc(), &candidates);
@@ -31,24 +30,20 @@ Profiler::calculateNonAdaptiveCandidateSet(
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-Profiler::calculateAdaptiveCandidateSet(
-  const Token& t,
-  csl::DictSearch::CandidateSet& candidates)
-{
+void Profiler::calculateAdaptiveCandidateSet(
+    const Token &t, csl::DictSearch::CandidateSet &candidates) {
   assert(config_.adaptive_);
   assert(t.has_metadata("correction"));
   assert(t.has_metadata("correction-lc"));
 
-  // std::wcerr << "(Profiler) Calculating adaptive profile for token "
-  // 	   << t.getWOCR() << " ("
-  // 	   << t.origin().metadata()["correction"] << ")\n";
+  std::wcerr << "(Profiler) Calculating adaptive profile for token "
+             << t.getWOCR() << " (" << t.metadata()["correction"] << ")\n";
   dictSearch_.query(t.getWOCR_lc(), &candidates);
   // for (const auto& cand: candidates) {
   // 	std::wcerr << "(Profiler) Adaptive candidate (before): "
   // 		<< cand << "\n";
   // }
-  candidates.discard_if([&t](const csl::Interpretation& i) {
+  candidates.discard_if([&t](const csl::Interpretation &i) {
     return i.getBaseWord() != t.metadata()["correction-lc"] and
            i.getWord() != t.metadata()["correction-lc"];
   });
@@ -64,18 +59,15 @@ Profiler::calculateAdaptiveCandidateSet(
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void
-Profiler::createCandidatesWithCorrection(
-  const Token& t,
-  csl::DictSearch::CandidateSet& candidates)
-{
+void Profiler::createCandidatesWithCorrection(
+    const Token &t, csl::DictSearch::CandidateSet &candidates) {
   assert(candidates.empty());
   // std::wcerr << "(Profiler) Calculating candidates from correction for token
   // "
   // 	   << t.getWOCR() << " ("
   // 	   << t.origin().metadata()["correction"] << ")\n";
-  csl::AdaptiveLex::add(
-    t.metadata()["correction-lc"], t.getWOCR_lc(), candidates);
+  csl::AdaptiveLex::add(t.metadata()["correction-lc"], t.getWOCR_lc(),
+                        candidates);
   // for (const auto& cand: candidates) {
   // 	std::wcerr << "(Profiler) AdaptiveGt candidate: " << cand << "\n";
   // }
